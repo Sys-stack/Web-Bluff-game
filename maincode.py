9458e7c45146a1608bfd3e95700810b58bdde7d5
Siv <shivasudharsank2008@gmail.com>
2025-03-19 20:36:40 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -3,6 +3,14 @@ import requests
 
 app = Flask(__name__)
 
+@app.route("/rooms")
+def rooms():
+    return "<h2>Welcome to the Rooms Page</h2>"
+
+@app.route("/credits")
+def credits():
+    return "<h2>Game Credits Page</h2>"
+    
 @app.route("/", methods=["POST", "GET"])
 def home():
     # Check which button was clicked
@@ -18,14 +26,6 @@ def home():
     response = requests.get(github_html_url)
 
     return response.text  # Returns the HTML content
-
-@app.route("/rooms")
-def rooms():
-    return "<h2>Welcome to the Rooms Page</h2>"
-
-@app.route("/credits")
-def credits():
-    return "<h2>Game Credits Page</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
