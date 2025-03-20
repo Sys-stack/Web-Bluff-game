34b8ec7de0d44686d6a114929e2d11e4079d1356
Siv <shivasudharsank2008@gmail.com>
2025-03-20 14:07:43 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -22,7 +22,7 @@ def home():
         return redirect(url_for("credits"))
 
     # Fetching HTML from GitHub (Not Recommended for Live Apps)
-    github_html_url = "https://cdn.jsdelivr.net/gh/Sys-stack/Web-Bluff-game@latest/Bluff.html"
+    github_html_url = "https://raw.githubusercontent.com/Sys-stack/Web-Bluff-game/refs/heads/main/Bluff.html"
     response = requests.get(github_html_url)
 
     return response.text  # Returns the HTML content
