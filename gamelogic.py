fd075c234128000a24b04ba3e0ccf63320c780cb
Siv <shivasudharsank2008@gmail.com>
2025-04-12 23:14:35 +0530

    Update gamelogic.py

--- gamelogic.py
+++ gamelogic.py
@@ -1,10 +1,12 @@
 import random
 from collections import defaultdict
 
-roomname = "yes"
-userids[roomname] = ["1sihs", "whhsjs", "eiehje", "ekejj"]
+#initialising variables
+
+
+
 class BluffGame:
-    def __init__(self, num_players=4, userids):
+    def __init__(self, userids, roomname, num_players=4):
         
         self.num_players = num_players
         self.userids = userids[roomname]
@@ -12,16 +14,22 @@ class BluffGame:
         self.suits = ['S', 'H', 'C', 'D']
         self.ranks = list(range(1, 14))  
         self.deck = [(suit, rank) for suit in self.suits for rank in self.ranks]
+        
         self.player_hands = {}
-        for i in userids:
+        for i in userids[roomname]:
             self.player_hands[i] = []
         self.current_player = 0
-        self.pass_count = 0
+        self.turn_count = 1
         self.last_play = {"player": -1, "claimed_rank": -1, "cards": []}
+        
         self.game_over = False
         
-        self.player_html = {}
+        self.turn = {"previous":[], "current":[], "played_previous":[], "played_current":[]}
         
+        self.player_html = {}
+        self.call_bluff = {}
+        for i in userids[roomname]:
+            self.call_bluff[i] = False
         for i in userids:
             self.player_html[i] = []
             
@@ -30,9 +38,9 @@ class BluffGame:
         
         while self.deck:
             index = len(self.deck) % self.num_players
-            self.player_hands[userids[index]].append(self.deck.pop())
+            self.player_hands[userids[roomname][index]].append(self.deck.pop())
             
-        for i in userids:
+        for i in userids[roomname]:
             self.player_hands[i].sort(key=lambda x: x[1])
     
     def sort_hand(self, userid):
@@ -47,21 +55,71 @@ class BluffGame:
             self.player_html[userid].append(f'<img class = "card" src = "https://raw.githubusercontent.com/Sys-stack/Web-Bluff-game/refs/heads/style-font-decor/Dark%20Cards/{rank}-{suit}.png" value = "{rank}{suit}"> ')
             
         
-    def play_hand(self, userid, played_hands):
-        for i in played_hands[userid]:
-        self.deck.append(self.player_hands[userid].pop(i))
+    def played(self, userid, played_hands):
+        for selected_cards in played_hands:
+
+            self.deck.append(selected_cards)
+            self.player_hands[userid].remove(selected_cards)
         
-    def call_bluff(self, userid, playedhands):
-        
+    def call_bluff(self, userid):
+        for check in self.call_bluff[userid]:
+            if check:
+                pass
+                if self.turn["previous"][1] != 's':
+                    pass
         
     
-    def play(self, userid):
-        print(self.player_hands[userid])
-        print(self.player_html[userid])
+    def start(self):
+        self.deal_cards()
+        for user in self.player_hands:
+            
+            if ('S', 1) in self.player_hands[user]:
+                starter = user
+        self.current_player = starter
+        self.userindex = self.userids.index(starter)
+        
+    def after_play(self, played_hands):
+        self.played(self.current_player, played_hands)
+        self.turn["previous_played"] = played_hands
+        
+        
+    def next_player_shift(self):
+        if self.turn_count < 13:
+            self.turn_count += 1
+        else:
+            self. turn_count = 1
+        
+        if self.userindex < 3:
+            self.userindex += 1
+        else:
+            self.userindex = 0
+        self.current_player = self.userids[self.userindex]
+        
+    def next_player_turned(self, played_hands):
+        pass
+        
         
     
 if __name__ == "__main__":
-    game = BluffGame()
-    game.deal_cards()
-    game.display_hand(userids[0])
-    game.play(userids[0])
+    userids = {}
+    roomname = "yes"
+    userids[roomname] = ["1sihs", "whhsjs", "eiehje", "ekejj"]
+
+    game = BluffGame(userids, roomname)
+    game.start()
+    
+    print(f"Starter is {game.current_player} and has the deck {game.player_hands[game.current_player]}")
+    
+    game.after_play([('S', 1)])
+    
+    print(f" afterplay: {game.player_hands[game.current_player]}")
+    
+    game.next_player_shift()
+    
+    print(f"Starter is {game.current_player} and has the deck {game.player_hands[game.current_player]}")
+    
+    game.next_player_turned([("C", 5)])
+    
+    
+        
+    
