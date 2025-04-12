c9bfd95a24129a313070e7f8c95bacc48cb52221
Siv <shivasudharsank2008@gmail.com>
2025-04-12 11:45:24 +0530

    Update gamelogic.py

--- gamelogic.py
+++ gamelogic.py
@@ -1,11 +1,13 @@
 import random
 from collections import defaultdict
 
-userids = ["1sihs", "whhsjs", "eiehje", "ekejj"]
+roomname = "yes"
+userids[roomname] = ["1sihs", "whhsjs", "eiehje", "ekejj"]
 class BluffGame:
-    def __init__(self, num_players=4):
+    def __init__(self, num_players=4, userids):
         
         self.num_players = num_players
+        self.userids = userids[roomname]
         
         self.suits = ['S', 'H', 'C', 'D']
         self.ranks = list(range(1, 14))  
@@ -19,6 +21,7 @@ class BluffGame:
         self.game_over = False
         
         self.player_html = {}
+        
         for i in userids:
             self.player_html[i] = []
             
@@ -39,15 +42,23 @@ class BluffGame:
             hand_by_rank[rank].append(suit)
 
     def display_hand(self, userid):
+        self.player_html[userid] = []
         for suit, rank in self.player_hands[userid]:
             self.player_html[userid].append(f'<img class = "card" src = "https://raw.githubusercontent.com/Sys-stack/Web-Bluff-game/refs/heads/style-font-decor/Dark%20Cards/{rank}-{suit}.png" value = "{rank}{suit}"> ')
             
-    def reset_html(self, userid):
-        self.player_html[userid] = []
+        
+    def play_hand(self, userid, played_hands):
+        for i in played_hands[userid]:
+        self.deck.append(self.player_hands[userid].pop(i))
+        
+    def call_bluff(self, userid, playedhands):
+        
+        
     
     def play(self, userid):
         print(self.player_hands[userid])
         print(self.player_html[userid])
+        
     
 if __name__ == "__main__":
     game = BluffGame()
